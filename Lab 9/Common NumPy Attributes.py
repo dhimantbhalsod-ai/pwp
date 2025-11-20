4eec39ba59d2a59226f65abf4a8ae9043628c36a
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:11:59 +0530

    Create Common NumPy Attributes.py

---
+++ Lab 9/Common NumPy Attributes.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+# create a 2-D array 
+array1 = np.array([[2, 4, 6],
+                  [1, 3, 5]])
+# check the dimension of array1
+print(array1.ndim)
+
+#NumPy Array size Attribute
+#The size attribute returns the total number of elements in the given array.
+
+import numpy as np
+array2 = np.array([[1, 2, 3],
+                 [6, 7, 8]])
+# return total number of elements in array1
+print(array2.size)
