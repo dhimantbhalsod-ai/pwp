f01257d12814c4042ae66a015e18b373d115e872
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:30:35 +0530

    Create pwp04 array sequencce.py

---
+++ Lab 9/pwp04 array sequencce.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  7 10:45:00 2025
+
+@author: student
+"""
+
+l1=[5,2.3,3+3j,'ICT']
+print(l1)
+print(l1[0])
+print(l1[1])
+print(l1[2])
+print(l1[3])
+print(type(l1[0]))
+print(type(l1[1]))
+print(type(l1[2]))
+print(type(l1[3]))
+l1[0]=8
+print(l1)
+print(type(l1[0]),type(l1[1]),type(l1[2]),type(l1[3]))
