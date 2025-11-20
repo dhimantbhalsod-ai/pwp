765f722a729febd019e628a908530867a79f49e1
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:16:49 +0530

    Create NumPy Type Conversion.py

---
+++ Lab 9/NumPy Type Conversion.py
@@ -0,0 +1,8 @@
+import numpy as np
+# create an array of integers
+int_array = np.array([1, 3, 5, 7])
+# convert data type of int_array to float
+float_array = int_array.astype('float')
+# print the arrays and their data types
+print(int_array, int_array.dtype)
+print(float_array, float_array.dtype)
