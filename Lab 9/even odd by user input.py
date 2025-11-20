e5fecf25cd6b06681d057a76ce29f8243767f96e
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:18:10 +0530

    Create even odd by user input.py

---
+++ Lab 9/even odd by user input.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 17 14:25:01 2025
+
+@author: slock
+"""
+
+# Even or Odd check
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(num, "is Even")
+else:
+    print(num, "is Odd")
