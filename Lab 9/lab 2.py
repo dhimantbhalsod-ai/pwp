0c1470ee03214bd110617bbcd297faa4337cdffe
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:19:35 +0530

    Create lab 2.py

---
+++ Lab 9/lab 2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 17 14:12:42 2025
+
+@author: slock
+"""
+a=int(input("enter the value of a = "))
+b=int(input("enter the value of b = "))
+
+
+Addition = (a + b)
+print("addition of a and b is ",Addition)
+
+substraction = (a-b)
+print("substraction of a and b is ",substraction)
+
+multiplication = a*b
+print("multiplication of a and b is ",multiplication)
+
+division = a/b
+print("division of a and b is ",division)
