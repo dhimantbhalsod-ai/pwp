abcbcce42a351af8acd0a4b60d0518899817c9ea
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:24:50 +0530

    Create pwp 01.py

---
+++ Lab 9/pwp 01.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  7 09:54:58 2025
+
+@author: student
+"""
+x=6
+print(x)
+print(type(x))
+x,y,z=7,8,9
+print('my variable x:',x)
+print('my variable y:',y)
+print('my variable z:',z)
