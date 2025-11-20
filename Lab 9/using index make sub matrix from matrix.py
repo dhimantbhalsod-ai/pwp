bdba0ed92e9fd41360217f047bd835feec8c632a
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:35:31 +0530

    Create using index make sub matrix from matrix.py

---
+++ Lab 9/using index make sub matrix from matrix.py
@@ -0,0 +1,5 @@
+import numpy as np
+z=np.array([[1,2,3],[4,5,6],[9,8,7]])
+print("this is matraix z \n\n",z)
+x = z[0:2,1:3] #in this first 0:2, is for colom,and other one is for row//
+print("\n",x)
