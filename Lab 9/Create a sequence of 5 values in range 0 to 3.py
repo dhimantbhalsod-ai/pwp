f62f3db7f86204d6b0b5b8e352a19523c89a6475
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:13:50 +0530

    Create Create a sequence of 5 values in range 0 to 3.py

---
+++ Lab 9/Create a sequence of 5 values in range 0 to 3.py
@@ -0,0 +1,7 @@
+#Create a Python Tuple
+
+import numpy as np
+
+# Example 4: Create a sequence of 5 values in range 0 to 3
+arr= np.linspace(0, 3, 5)
+print ("A sequential array with 5 values between 0 and 5:\n", arr)
