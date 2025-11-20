41cc2c8a0ffaccd71c9182dda14f42e950886665
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:30:59 +0530

    Create pwp05.py

---
+++ Lab 9/pwp05.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  9 13:50:17 2025
+
+@author: OM
+"""
+
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
