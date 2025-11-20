153f3f1c83971cec4fbc4b7fc1148941aea6d347
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:28:30 +0530

    Create pwp 06.py

---
+++ Lab 9/pwp 06.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  9 13:53:40 2025
+
+@author: OM
+"""
+
+
+print("a", "a^2", "a^3")
+print(1, 1**2, 1**3)
+print(2, 2**2, 2**3)
+print(3, 3**2, 3**3)
+print(4, 4**2, 4**3)
