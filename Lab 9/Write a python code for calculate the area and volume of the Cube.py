730bd970349dddb90f354c894952f1311cf2bff0
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:17:40 +0530

    Create Write a python code for calculate the area and volume of the Cube.py

---
+++ Lab 9/Write a python code for calculate the area and volume of the Cube.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 17 14:26:15 2025
+
+@author: slock
+"""
+
+# Area and Volume of a Cube
+side = float(input("Enter the side length of the cube: "))
+
+area_cube = 6 * side * side
+volume_cube = side ** 3
+
+print("Surface Area of Cube =", area_cube)
+print("Volume of Cube =", volume_cube)
