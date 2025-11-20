a37e42e41f1aa578f3e13adbd5b548c58adc5c3a
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:23:07 +0530

    Create multiply 2 matrix using python.py

---
+++ Lab 9/multiply 2 matrix using python.py
@@ -0,0 +1,9 @@
+import numpy as np
+z=np.array([[1,2,3],[4,5,6],[9,8,7]])
+print("this is matraix z \n\n",z)
+
+y=np.array([[5,7,8],[2,3,4],[6,7,8]])
+print("this is matraix y \n\n",y)
+
+x=z*y
+print("\n",x)
