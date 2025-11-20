ae8894e5fa2596e96df16c7834f14edafaa5e9b2
dhimantbhalsod-ai <dhimantbhalsod@gmail.com>
2025-11-20 16:18:42 +0530

    Create how to print digit using jump.py

---
+++ Lab 9/how to print digit using jump.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 10 14:44:18 2025
+
+@author: slock
+"""
+
+s1="ICT IT CE"
+print(s1)
+print(s1[2]+s1[4])
+print(s1[0::2])
+"""if we want to jump digit we use :: to it"""
